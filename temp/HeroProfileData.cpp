7308796e6384b314347cca2ded3d2c58a68205a1
vijaybabuvcp-eng <vijaybabu.vcp@gmail.com>
2025-11-28 10:19:34 -0500

    y

--- temp/HeroProfileData.cpp
+++
@@ -1,6 +0,0 @@
-// HeroProfileData.cpp
-
-#include "HeroProfileData.h"
-#include "Animation/AnimInstance.h"
-#include "Engine/SkeletalMesh.h"
-#include "UnitTypeData.h"
--- temp/RomanEraGame.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RomanEraGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RomanEraGame, "RomanEraGame" );
-
-DEFINE_LOG_CATEGORY(LogRomanEraGame)
